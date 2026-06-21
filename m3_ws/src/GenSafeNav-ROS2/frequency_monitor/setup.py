8fa837cc10007ff02b5e64d739d1b3299cef28db
pavansai018 <18pavansai@gmail.com>
2026-06-21 22:59:25 +0800

    Package for detecting pedestrians using DROW3 or DR-SPAAM in ROS 2.

---
+++ m3_ws/src/GenSafeNav-ROS2/frequency_monitor/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+package_name = 'frequency_monitor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    # If your source code is under 'src/', uncomment the next line
+    # package_dir={'': 'src'},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # (os.path.join('share', package_name, 'model_weight'), glob('model_weight/*')),
+    ],
+    install_requires=['setuptools', 'numpy'],
+    zip_safe=True,
+    maintainer='pavansai018',
+    maintainer_email='18pavansai@gmail.com',
+    description='Package for detecting pedestrians using DROW3 or DR-SPAAM in ROS 2.',
+    license='BSD-3-Clause',
+    extras_require={
+        "test": ["pytest"],
+    },
+    entry_points={
+        'console_scripts': [
+            'frequency_monitor = frequency_monitor.main:main',
+        ],
+    },
+)
